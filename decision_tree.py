06bb6874573330644880a25989e40fcc5dd90fca
Kyusw <aganus@hawk.iit.edu>
2025-02-22 04:12:45 +0900

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.tree import export_text
 
 df = pd.read_csv("combined_district_data_cong.csv")
 
@@ -21,5 +22,15 @@ dt = DecisionTreeClassifier(criterion="gini", random_state=42)
 dt.fit(X_train, y_train)
 y_pred = dt.predict(X_test)
 
+feature_importances = pd.DataFrame({'Feature': X.columns, 'Importance': dt.feature_importances_})
+feature_importances = feature_importances.sort_values(by="Importance", ascending=False).head(10)
+
+print("Features:")
+print(feature_importances)
+
+tree_rules = export_text(dt, feature_names=list(X.columns))
+print(tree_rules)
+
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
+
